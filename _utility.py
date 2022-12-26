def603ef1daf0fe6de14e94cec0e58f375ec2ac8
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2022-12-26 19:12:48 +0000

    Ensure tokens are stored in the correct row

--- _utility.py
+++ _utility.py
@@ -1,3 +1,12 @@
 class gl:
     pkl_df_jobs_file = "pkl_df_jobs.pkl"
     pkl_df_selected_jobs = "pkl_df_selected_jobs.pkl"
+    # col_names =  ['Id', 'Title', 'Body', 'Bullet_Points', 'Score']
+    id = "Id"
+    title = "Title"
+    body = "Body"
+    bullet_points = 'Bullet_Points'
+    score = "Score"
+    title_tokens = "Title_Tokens"
+    body_tokens = "Body Tokens"
+    bullet_point_tokens = 'Bullet_Point Tokens'
