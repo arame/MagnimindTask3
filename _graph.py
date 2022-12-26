7010611c744b876507e435571060a7886da7dd64
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2022-12-26 20:18:04 +0000

    Show wordclouds

--- _graph.py
+++ _graph.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 def histplot_count(title, df, x):
     p = sns.histplot(data=df, x=x, stat='count')
@@ -8,3 +9,12 @@ def histplot_count(title, df, x):
 def histplot_range_count(title, df, x, range):
     p = sns.histplot(data=df, x=x, stat='count', binrange=range)
     plt.title(title)
+
+def show_word_cloud(text, title):
+    wordcloud = WordCloud(collocations = False, background_color = 'white').generate(text)
+    # Display the generated image:
+    plt.figure(figsize = (10, 8), facecolor = None)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.title(title)
+    plt.show()
