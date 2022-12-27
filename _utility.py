5e30790342721ef1f0558fae42847377b2ab68a1
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2022-12-27 11:23:27 +0000

    Show percentages in summary statistics

--- _utility.py
+++ _utility.py
@@ -1,6 +1,7 @@
 class gl:
     pkl_df_jobs_file = "pkl_df_jobs.pkl"
     pkl_df_selected_jobs = "pkl_df_selected_jobs.pkl"
+    pkl_df_not_selected_jobs = "pkl_df_not_selected_jobs.pkl"
     # col_names =  ['Id', 'Title', 'Body', 'Bullet_Points', 'Score']
     id = "Id"
     title = "Title"
